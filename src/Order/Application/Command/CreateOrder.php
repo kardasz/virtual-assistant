be7c51cecaee996eaf3cfafc2370319e9ca2c925
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-03-20 08:37:21 +0100

    Create order

---
+++ src/Order/Application/Command/CreateOrder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Order\Application\Command;
+
+use App\Shared\Domain\Command\CommandInterface;
+use App\Shared\Domain\ValueObject\OrderId;
+
+readonly class CreateOrder implements CommandInterface
+{
+    public function __construct(
+        public OrderId $orderId,
+    ) {
+    }
+}
