9731bdc23604686db029331c7995371c670d5027
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-03-23 09:48:03 +0100

    Update CreateOrder.php

--- src/Order/Application/Command/CreateOrder.php
+++ src/Order/Application/Command/CreateOrder.php
@@ -10,6 +10,7 @@ readonly class CreateOrder implements CommandInterface
     public function __construct(
         public OrderId $orderId,
         public string $title,
+        public string $description,
     ) {
     }
 }
