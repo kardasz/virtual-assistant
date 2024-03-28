99b4f2fe28ab61ed025e2238e22cf7039736cca1
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-03-28 07:59:32 +0100

    CreateOrder Handler

---
+++ src/Order/Application/CommandHandler/CreateOrderHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Order\Application\CommandHandler;
+
+use App\Order\Domain\Entity\Order;
+use App\Shared\Domain\Command\CommandHandlerInterface;
+use App\Shared\Domain\ValueObject\OrderId;
+use App\Order\Application\Command\CreateOrder;
+use App\Order\Domain\Repository\OrderRepositoryInterface;
+
+readonly class CreateOrderHandler implements CommandHandlerInterface
+{
+    public function __construct(
+        private OrderRepositoryInterface $orderRepository,
+    ) {
+    }
+
+    public function __invoke(CreateOrder $command): Order
+    {
+        $order = Order::createNew(
+            orderId: OrderId::generate(),
+        );
+        $this->orderRepository->persist($order);
+
+        return $order;
+    }
+}
