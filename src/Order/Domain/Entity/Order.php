77f839e4d73bcb70c79f868ab4b0b697bd9250b3
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-04-02 10:39:02 +0200

    Expires at

--- src/Order/Domain/Entity/Order.php
+++ src/Order/Domain/Entity/Order.php
@@ -12,6 +12,7 @@ class Order extends AggregateRoot
     public readonly string $firstName;
     public readonly string $lastName;
     public readonly string $email;
+    public readonly \DateTimeImmutable $expiresAt;
     
     public static function createNew(
         OrderId $orderId,
