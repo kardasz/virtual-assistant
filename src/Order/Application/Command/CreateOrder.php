4dcf2af9fd6bbfbe989a285415a5b7fea3920a36
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-04-16 09:11:47 +0200

    Update

--- src/Order/Application/Command/CreateOrder.php
+++ src/Order/Application/Command/CreateOrder.php
@@ -3,13 +3,14 @@
 namespace App\Order\Application\Command;
 
 use App\Shared\Domain\Command\CommandInterface;
+use App\Shared\Domain\ValueObject\CustomerId;
 use App\Shared\Domain\ValueObject\OrderId;
 
 readonly class CreateOrder implements CommandInterface
 {
     public function __construct(
         public OrderId $orderId,
-        public string $orderingPartyId,
+        public CustomerId $orderingPartyId,
         public string $title,
         public string $description,
         public \DateTimeImmutable $expiresAt,
