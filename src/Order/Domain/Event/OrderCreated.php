e516dc8a637d06e4e93f38ba129e0f4b323eea1c
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-04-25 18:20:52 +0200

    Update

--- src/Order/Domain/Event/OrderCreated.php
+++ src/Order/Domain/Event/OrderCreated.php
@@ -3,12 +3,16 @@
 namespace App\Order\Domain\Event;
 
 use App\Shared\Domain\Event\DomainEvent;
+use App\Shared\Domain\ValueObject\CustomerId;
 use App\Shared\Domain\ValueObject\OrderId;
 
 class OrderCreated extends DomainEvent
 {
     public readonly OrderId $orderId;
     public readonly string $title;
+    public readonly CustomerId $orderingPartyId;
+    public readonly string $description;
+    public readonly \DateTimeImmutable $expiresAt;
 
     public static function withData(
         OrderId $orderId,
