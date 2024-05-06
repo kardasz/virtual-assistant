7d8677beda8931bf557811b2a9dc0775fb839916
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-05-06 20:03:53 +0200

    Update Calendar.php

--- src/Calendar/Domain/Entity/Calendar.php
+++ src/Calendar/Domain/Entity/Calendar.php
@@ -9,7 +9,7 @@ use App\Shared\Domain\ValueObject\CalendarId;
 class Calendar extends AggregateRoot
 {
     public CalendarId $CalendarId;
-    public string $title;
+    public string $title2;
     public bool $repeat;
     public \DateTimeImmutable $createdAt
 
